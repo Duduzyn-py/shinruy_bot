7f134f5fd735a9e5979829e40c1828b35af00bca
Eduardo <eduardocaitano15@gmail.com>
2022-09-26 11:12:07 -0300

    Im at a payphone trying to call home

---
+++ backend/main.py
@@ -0,0 +1,31 @@
+"""
+repositório de referência:
+https://github.com/fribbels/Fribbels-Epic-7-Optimizer
+
+As classes:
+1.Adicionar gear
+2.Calcular GS(Gear Score)
+3.Listar gear
+4.Signos(especificidades de status recebido e afins)
+5.Montar build para heróis
+https://raw.githubusercontent.com/fribbels/Fribbels-Epic-7-Optimizer/main/e7dbherodata.json
+json com os heróis
+6.Funções do login e tals
+
+
+Funções:
+1.Add gear)
+    Inserir datatype(str):
+        tipo(sword, helmet, armor, necklace, ring e boot)
+        main sattus, para os itens da direita(Atributo % e flat, speed exclusivo para botas)
+        substatus(atk%, def%, hp%, eff%, er%, cc, cdm, atk, def, hp, spd)
+        set()
+
+2.Remover gear pique um "card", ai tera botão de mandar tchau item
+   
+
+
+Diagramas:
+https://www.planttext.com/
+
+"""
